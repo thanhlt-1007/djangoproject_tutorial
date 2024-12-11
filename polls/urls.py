224332e9b7d6887e1e5c1cad6837839246ceb949
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-11 14:58:10 +0700

    Add polls votes view

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:question_id>", views.detail, name="detail"),
-    path("<int:question_id>/results", views.results, name="results")
+    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:question_id>/votes", views.results, name="votes"),
 ]
