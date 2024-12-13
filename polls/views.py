1de9d121bb0328c4beb23a7f3966d013681c25f3
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-13 11:37:51 +0700

    Init polls/index template

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
 
-from .models import Question
+from polls.models import Question
 
+
+import pdb
 
 def index(request):
     latest_questions = Question.objects.order_by("-pub_date")[:5]
-    question_texts = [question.question_text for question in latest_questions]
-    content = ", ".join(question_texts)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_questions": latest_questions,
+    }
+    content = template.render(context=context, request=request)
     return HttpResponse(content)
 
 
