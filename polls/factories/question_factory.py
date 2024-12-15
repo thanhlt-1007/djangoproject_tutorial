9b8cac07891edc11ee1aeb6490cec7b39f701c6d
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-16 00:07:06 +0700

    Add QuestionFactory

---
+++ polls/factories/question_factory.py
@@ -0,0 +1,10 @@
+from factory import Faker
+from factory.django import DjangoModelFactory
+
+
+class QuestionFactory(DjangoModelFactory):
+    class Meta:
+        model = "polls.Question"
+
+    question_text = Faker("sentence")
+    pub_date =Faker("date_object")
