f72a774ceb86bb46e81e26a0327bf0afa1302eaa
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-12-16 12:44:13 +0700

    Add PollsResultsView

--- polls/urls.py
+++ polls/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "polls"
 urlpatterns = [
     path("", views.PoolsListView.as_view(), name="list"),
-    path("<int:pk>", views.PollsDetailViiew.as_view(), name="detail"),
-    path("<int:question_id>/results", views.results, name="results"),
-    path("<int:question_id>/votes", views.results, name="votes"),
+    path("<int:pk>", views.PollsDetailView.as_view(), name="detail"),
+    path("<int:pk>/results", views.PollsResultsView.as_view(), name="results"),
+    path("<int:question_id>/votes", views.votes, name="votes"),
     path("<int:question_id>/vote", views.vote, name="vote"),
 ]
